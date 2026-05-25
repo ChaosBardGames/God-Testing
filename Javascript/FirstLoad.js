c9c5f7c0dcb5ed32c39505b65a0368f9740b75a6
Call me Jerry <GarathX2@outlook.com>
2026-05-25 11:49:18 +0000

    Add FirstLoad function to manage page visibility

--- Javascript/FirstLoad.js
+++ Javascript/FirstLoad.js
@@ -1,1 +1,8 @@
-
+function FirstLoad() {
+  const FirstPage = document.getElementByClass("Title Card");
+  const SecondPage = document.getElementByClass("Page Two");
+  const ThirdPage = document.getElementByClass("Page Three");
+  FirstPage.style.display = "block";
+  SecondPage.style.display = "none";
+  ThirdPage.style.display = "none";  
+}
