f9ad1607d266acea48f06d921dad49d1772ff5d8
Call me Jerry <GarathX2@outlook.com>
2026-05-25 12:10:10 +0000

    Fix element selection method from getElementByClass to getElementById

--- Javascript/Page_Swapper.js
+++ Javascript/Page_Swapper.js
@@ -1,25 +1,25 @@
 function First_Page() {
-  const FirstPage = document.getElementByClass("Title Card");
-  const SecondPage = document.getElementByClass("Page Two");
-  const ThirdPage = document.getElementByClass("Page Three");
+  const FirstPage = document.getElementById("Title Card");
+  const SecondPage = document.getElementById("Page Two");
+  const ThirdPage = document.getElementById("Page Three");
   FirstPage.style.display = "block";
   SecondPage.style.display = "none";
   ThirdPage.style.display = "none";  
 }
 
 function Second_Page() {
-  const FirstPage = document.getElementByClass("Title Card");
-  const SecondPage = document.getElementByClass("Page Two");
-  const ThirdPage = document.getElementByClass("Page Three");
+  const FirstPage = document.getElementById("Title Card");
+  const SecondPage = document.getElementById("Page Two");
+  const ThirdPage = document.getElementById("Page Three");
   FirstPage.style.display = "none";
   SecondPage.style.display = "block";
   ThirdPage.style.display = "none";  
 }
 
 function Third_Page() {
-  const FirstPage = document.getElementByClass("Title Card");
-  const SecondPage = document.getElementByClass("Page Two");
-  const ThirdPage = document.getElementByClass("Page Three");
+  const FirstPage = document.getElementById("Title Card");
+  const SecondPage = document.getElementById("Page Two");
+  const ThirdPage = document.getElementById("Page Three");
   FirstPage.style.display = "none";
   SecondPage.style.display = "none";
   ThirdPage.style.display = "block";  
