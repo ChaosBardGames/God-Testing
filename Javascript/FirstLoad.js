4e8a7d1149fdd9d5f700b5fc83820f922bd4fecc
Call me Jerry <GarathX2@outlook.com>
2026-05-25 12:04:37 +0000

    Fix element selection method in FirstLoad function

--- Javascript/FirstLoad.js
+++ Javascript/FirstLoad.js
@@ -1,7 +1,7 @@
 function FirstLoad() {
-  const FirstPage = document.getElementByClass("Title Card");
-  const SecondPage = document.getElementByClass("Page Two");
-  const ThirdPage = document.getElementByClass("Page Three");
+  const FirstPage = document.getElementById("Title Card");
+  const SecondPage = document.getElementById("Page Two");
+  const ThirdPage = document.getElementById("Page Three");
   FirstPage.style.display = "block";
   SecondPage.style.display = "none";
   ThirdPage.style.display = "none";  
