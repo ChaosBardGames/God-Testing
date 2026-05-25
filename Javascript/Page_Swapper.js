c208d565fd6e8c6ddc6be493872982bdb6fb46fe
Call me Jerry <GarathX2@outlook.com>
2026-05-25 12:17:43 +0000

    Update element IDs for page swapping functions

--- Javascript/Page_Swapper.js
+++ Javascript/Page_Swapper.js
@@ -1,25 +1,25 @@
 function First_Page() {
-  const FirstPage = document.getElementById("Title Card");
-  const SecondPage = document.getElementById("Page Two");
-  const ThirdPage = document.getElementById("Page Three");
+  const FirstPage = document.getElementById("Title_Card");
+  const SecondPage = document.getElementById("Page_Two");
+  const ThirdPage = document.getElementById("Page_Three");
   FirstPage.style.display = "block";
   SecondPage.style.display = "none";
   ThirdPage.style.display = "none";  
 }
 
 function Second_Page() {
-  const FirstPage = document.getElementById("Title Card");
-  const SecondPage = document.getElementById("Page Two");
-  const ThirdPage = document.getElementById("Page Three");
+  const FirstPage = document.getElementById("Title_Card");
+  const SecondPage = document.getElementById("Page_Two");
+  const ThirdPage = document.getElementById("Page_Three");
   FirstPage.style.display = "none";
   SecondPage.style.display = "block";
   ThirdPage.style.display = "none";  
 }
 
 function Third_Page() {
-  const FirstPage = document.getElementById("Title Card");
-  const SecondPage = document.getElementById("Page Two");
-  const ThirdPage = document.getElementById("Page Three");
+  const FirstPage = document.getElementById("Title_Card");
+  const SecondPage = document.getElementById("Page_Two");
+  const ThirdPage = document.getElementById("Page_Three");
   FirstPage.style.display = "none";
   SecondPage.style.display = "none";
   ThirdPage.style.display = "block";  
